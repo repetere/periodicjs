b3eba4cdc10366d912a0aeec47d75325e54b4d11
Yaw Joseph Etse <yaw.etse@gmail.com>
2014-06-02 19:49:33 -0400

    working logs

--- app/lib/periodic.js
+++ app/lib/periodic.js
@@ -41,11 +41,12 @@ var express = require('express'),
 	ejs = require('ejs'),
 	app = express(),
 	expressAppLogger = require('morgan'),
+	appLog = require('../../content/config/logger'),
 	config = require('./config'),
-	appconfig;
+	appconfig,
+	logger;
 
 //https://github.com/expressjs/csurf
-//https://github.com/expressjs/compression
 //https://github.com/expressjs/timeout
 //https://github.com/expressjs/vhost
 //https://github.com/senchalabs/connect/blob/master/Readme.md#middleware
@@ -54,7 +55,7 @@ var init = {
 	loadConfiguration : function(){
 		appconfig = new config();
 		if(appconfig.settings().debug){
-			console.log(appconfig.settings());
+			logger.debug(appconfig.settings());
 		}
 		app.set('port',appconfig.settings().application.port);
 		app.set('env',appconfig.settings().application.environment);
@@ -72,12 +73,8 @@ var init = {
 		app.use(favicon( path.resolve(__dirname,'../../public/favicon.ico') ) );
 	},
 	staticCaching : function(){
-		if(app.get('env') !== 'production'){
-			app.use(express.static(path.resolve(__dirname,'../../public')));
-		}
-		else{
-			app.use(express.static(path.resolve(__dirname,'../../public'),{maxAge: 86400000}));
-		}
+		var expressStaticOptions = (app.get('env') !== 'production') ? {} : {maxAge: 86400000};
+		app.use(express.static(path.resolve(__dirname,'../../public'),expressStaticOptions));
 	},
 	userAuth : function(){
 
@@ -97,34 +94,55 @@ var init = {
 
 	},
 	logErrors : function(){
+		logger = new appLog(app.get('env'));
+
 		app.use(function(req,res,next){
 			console.log('%s %s',req.method,req.url);
 			next();
+		});
+
+		//log errors
+		app.use(function(err, req, res, next){
+			logger.error(err.stack);
+			next(err);
+		});
+
+		//send client errors
+		//catch all errors
+		app.use(function (err, req, res, next) {
+			// console.log("err.name",err.name);
+			if (req.xhr) {
+				res.send(500, { error: 'Something blew up!' });
+			}
+			else {
+				res.status(500);
+				res.render('errors/500', { error: err });
+			}
 		});
 	},
 	appLogging : function(){
 		app.use(expressAppLogger({ format: 'dev', immediate: true }));
 	},
 	applicationRouting : function(){
-		app.get('*',function(req,res){
+		app.get('/',function(req,res){
 			console.log("got it");
 			res.render('home/index',{randomdata:'twerkin'});
 		});
 	},
 	serverStatus: function(){
-		console.log('Express server listening on port ' + app.get('port'));
-		console.log('Running in environment: '+app.get('env'));
-		// expressAppLogger.logger('looks good');
+		logger.info('Express server listening on port ' + app.get('port'));
+		logger.info('Running in environment: '+app.get('env'));
+		logger.silly('looks good.');
 	}
 };
 
+init.logErrors();
 init.loadConfiguration();
 init.viewSettings();
 init.expressSettings();
 init.staticCaching();
 init.pageCaching();
 init.useLocals();
-init.logErrors();
 init.applicationRouting();
 init.serverStatus();
 
