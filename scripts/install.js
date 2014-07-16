2e233825382b73c7185510eb46fdd52296f93d16
Yaw Joseph Etse <yaw.etse@gmail.com>
2014-07-16 00:38:55 -0400

    new npm install

--- scripts/install.js
+++ scripts/install.js
@@ -8,14 +8,67 @@
 'use strict';
 
 var fs = require('fs-extra'),
-		path = require('path');
-fs.move(path.resolve(process.cwd(),'node_modules','periodicjs'),path.resolve(process.cwd(),'periodicjs'),function(err){
-	if(err){
+		path = require('path'),
+		prompt = require('prompt'),
+		originalnodemoduleslocation = path.resolve(process.cwd(),'../../node_modules'),
+		originallocation = path.resolve(process.cwd(),'../../node_modules','periodicjs'),
+		newlocation = path.resolve(process.cwd(),'../../periodicjs'),
+		schema = {
+	    properties: {
+	      auto_clean_up: {
+	        message: 'Do you want to remove node_modules and copy periodicjs to site root? (Yes or No)',
+	        required: true
+	      }
+	    }
+	  };
+
+
+prompt.start();
+prompt.get(schema, function (err, result) {
+  if(err){
 		console.error(err);
 		process.exit(0);
 	}
 	else{
-		console.log("Installed Periodicjs");
-		process.exit(0);
+		if(result.auto_clean_up.match(/y/gi)){
+			// console.log("move",originallocation);
+			// console.log("to",newlocation);
+
+			fs.ensureDir(newlocation,function(err){
+				if(err){
+					console.error(err);
+					process.exit(0);
+				}
+				else{
+					fs.copy(
+						originallocation,
+						newlocation,
+						function(err){
+						if(err){
+							console.error(err);
+							console.log(err.stack);
+							process.exit(0);
+						}
+						else{
+							fs.remove(originalnodemoduleslocation, function(err){
+								if(err){
+									console.error(err);
+									console.log(err.stack);
+									process.exit(0);
+								}
+								else{	
+									console.log("Installed Periodicjs");
+									process.exit(0);
+								}
+							});
+						}
+					});
+				}
+			});
+		}
+		else{
+			console.log("Installed Periodicjs");
+			process.exit(0);
+		}
 	}
-});
+});		
