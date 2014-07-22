2df503ea4fc472b747e68d235ff2f5ca7aa84d01
Yaw Joseph Etse <yaw.etse@gmail.com>
2014-07-22 16:21:59 -0400

    automated extension generation

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,7 +5,8 @@
  * Copyright (c) 2014 Yaw Joseph Etse. All rights reserved.
  */
 'use strict';
-var path = require('path');
+var path = require('path'),
+	uglify = require('uglify-js');
 
 module.exports = function(grunt) {
 	grunt.initConfig({
@@ -58,25 +59,9 @@ module.exports = function(grunt) {
         'package.json',
         'config/**/*.js',
         'content/extensions/node_modules/**/index.js',
-        'content/extensions/node_modules/**/contoller/*.js',
+        'content/extensions/node_modules/**/contoller/**/*.js',
         'content/extensions/node_modules/**/resources/*.js',
         '!content/extensions/node_modules/**/node_modules/**/*.js'
-        // 'content/extensions/node_modules/periodicjs.ext.admin/index.js',
-        // 'content/extensions/node_modules/periodicjs.ext.dbseed/contoller/*.js',
-        // 'content/extensions/node_modules/periodicjs.ext.dbseed/index.js',
-        // 'content/extensions/node_modules/periodicjs.ext.default_routes/contoller/*.js',
-        // 'content/extensions/node_modules/periodicjs.ext.default_routes/index.js',
-        // 'content/extensions/node_modules/periodicjs.ext.install/resources/*.js',
-        // 'content/extensions/node_modules/periodicjs.ext.install/contoller/*.js',
-        // 'content/extensions/node_modules/periodicjs.ext.install/index.js',
-        // 'content/extensions/node_modules/periodicjs.ext.login/contoller/*.js',
-        // 'content/extensions/node_modules/periodicjs.ext.login/index.js',
-        // 'content/extensions/node_modules/periodicjs.ext.mailer/contoller/*.js',
-        // 'content/extensions/node_modules/periodicjs.ext.mailer/index.js',
-        // 'content/extensions/node_modules/periodicjs.ext.scheduled_content/index.js',
-        // 'content/extensions/node_modules/periodicjs.ext.user_access_control/contoller/*.js',
-        // 'content/extensions/node_modules/periodicjs.ext.user_access_control/model/*.js',
-        // 'content/extensions/node_modules/periodicjs.ext.user_access_control/index.js'
       ]
 		},
 		jsdoc: {
@@ -94,30 +79,42 @@ module.exports = function(grunt) {
 					expand: true,
 					cwd: 'content/extensions/node_modules',
 					src: ['**/resources/js/*_src.js'],
-					dest: 'public/extensions',
+					dest: 'content/extensions/node_modules',
+					// dest: 'public/extensions',
 					rename: function(dest, src) {
 						var finallocation = path.join(dest, src);
-						finallocation = finallocation.replace("_src", "");
-						finallocation = finallocation.replace("resources", "");
+						finallocation = finallocation.replace("_src", "_build");
+						finallocation = finallocation.replace("resources", "public");
 						finallocation = path.resolve(finallocation);
 						// console.log("dest", dest, "src", src, "finallocation", finallocation);
 						return finallocation;
 					}
         }],
-				options: {
-					transform: ['uglifyify']
-				}
+				options: {}
 			}
 		},
 		uglify: {
-			my_target: {
-				options: {
-					sourceMap: true,
-					sourceMapName: 'public/scripts/index-sourcemap.map'
-				},
-				files: {
-					'public/scripts/index.min.js': ['public/scripts/index.js']
+			options: {
+				sourceMap: true,
+				compress: {
+					drop_console: false,
 				}
+			},
+			all: {
+				files: [{
+					expand: true,
+					cwd: 'content/extensions/node_modules',
+					src: ['**/public/js/*_build.js'],
+					dest: 'content/extensions/node_modules',
+					// dest: 'public/extensions',
+					rename: function(dest, src) {
+						var finallocation = path.join(dest, src);
+						finallocation = finallocation.replace("_build", ".min");
+						finallocation = path.resolve(finallocation);
+						// console.log("dest", dest, "src", src, "finallocation", finallocation);
+						return finallocation;
+					}
+        }]
 			}
 		},
 		less: {
@@ -132,13 +129,32 @@ module.exports = function(grunt) {
 				}
 			}
 		},
-		cssmin: {
-			combine: {
-				files: {
-					'public/styles/manuscript.min.css': ['public/styles/manuscript.css']
-				}
+		copy: {
+			main: {
+				files: [
+					{
+						expand: true,
+						cwd: 'content/extensions/node_modules',
+						src: ['**/public/**/*.*', '!**/public/**/*_build.js', '!**/node_modules/**/*.*'],
+						dest: 'public/extensions/',
+						rename: function(dest, src) {
+							var finallocation = path.join(dest, src.replace("public", ""));
+							// finallocation = finallocation;
+							finallocation = path.resolve(finallocation);
+							// console.log("dest", dest, "src", src, "finallocation", finallocation);
+							return finallocation;
+						}
+          }
+        ]
 			}
 		},
+		// cssmin: {
+		// 	combine: {
+		// 		files: {
+		// 			'public/styles/manuscript.min.css': ['public/styles/manuscript.css']
+		// 		}
+		// 	}
+		// },
 		// imagemin: {                          // Task
 		//   dynamic: {                         // Another target
 		//     options: {                       // Target options
@@ -155,7 +171,7 @@ module.exports = function(grunt) {
 		watch: {
 			scripts: {
 				files: ["<%= jshint.all %>"],
-				tasks: ['lint', 'packagejs', 'doc', 'css', 'test', 'less'],
+				tasks: ['lint', 'packagejs', 'beautify', 'minjs', 'doc', 'css', 'test', 'css', 'copypublic'],
 				options: {
 					interrupt: true
 				}
@@ -171,13 +187,16 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-less');
 	grunt.loadNpmTasks('grunt-jsdoc');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
 	grunt.loadNpmTasks('grunt-newer');
 
 	grunt.registerTask('default', ['lint', 'browserify', 'doc', 'cssmin', 'uglify', 'test', 'less']);
-	grunt.registerTask('lint', ['newer:jshint', 'newer:jsbeautifier']);
+	grunt.registerTask('lint', 'newer:jshint:all');
+	grunt.registerTask('beautify', 'newer:jsbeautifier');
 	grunt.registerTask('test', 'newer:simplemocha');
 	grunt.registerTask('packagejs', 'newer:browserify');
+	grunt.registerTask('copypublic', 'newer:copy');
 	grunt.registerTask('minjs', 'newer:uglify');
 	grunt.registerTask('css', 'newer:less');
 	grunt.registerTask('doc', 'newer:jsdoc');
