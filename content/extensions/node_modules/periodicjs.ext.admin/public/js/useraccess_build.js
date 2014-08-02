4f6f3b81114448bba3be30feb582ddba6d3e4162
Yaw Joseph Etse <yaw.etse@gmail.com>
2014-08-01 20:09:17 -0400

    editing user profiles

--- content/extensions/node_modules/periodicjs.ext.admin/public/js/useraccess_build.js
+++ content/extensions/node_modules/periodicjs.ext.admin/public/js/useraccess_build.js
@@ -2126,6 +2126,7 @@ module.exports = function(arr, fn, initial){
 'use strict';
 
 var letterpress = require('letterpressjs'),
+	request = require('superagent'),
 	privileges_lp = new letterpress({
 		idSelector : '#padmin-privileges',
 		sourcedata: '/userprivileges/search.json',
@@ -2140,16 +2141,52 @@ var letterpress = require('letterpressjs'),
 				callback(id,val);
 			}
 		}
+	}),
+	deleteButton;
+
+var deleteUserRole = function(e) {
+	e.preventDefault();
+	var eTarget = e.target;
+	request
+		.post(eTarget.getAttribute('data-href'))
+		.set('Accept', 'application/json')
+		.send({ 
+			_csrf: document.querySelector('input[name=_csrf]').value 
+		})
+		.query({ format: 'json'})
+		.end(function(error, res){
+			if(res.error){
+				error = res.error;
+			}
+			if(error || res.status === 500){
+				window.ribbonNotification.showRibbon( error.message,4000,'error');
+			}
+			else{
+				if(res.body.result==='error'){
+					window.ribbonNotification.showRibbon( res.body.data.error,4000,'error');
+				}
+				else{
+					window.ribbonNotification.showRibbon( res.body.data ,4000,'warn');
+					// var assetid = eTarget.getAttribute("assetid");
+					// var assettr = document.querySelector('[data-tr-assetid="'+assetid+'"]');
+					// removeTableRow(assettr);
+				}
+			}
 	});
+};
 
 window.addEventListener('load',function(){
 	privileges_lp.init();
+	deleteButton = document.getElementById('delete-userrole');
 	if(typeof window.userroleprivileges ==='object'){
 		privileges_lp.setPreloadDataObject(window.userroleprivileges);
 	}
 	window.ajaxFormEventListers('._pea-ajax-form');
+	if(deleteButton){
+		deleteButton.addEventListener('click',deleteUserRole,false);
+	}
 });
-},{"letterpressjs":1}],12:[function(require,module,exports){
+},{"letterpressjs":1,"superagent":8}],12:[function(require,module,exports){
 // Copyright Joyent, Inc. and other Node contributors.
 //
 // Permission is hereby granted, free of charge, to any person obtaining a
