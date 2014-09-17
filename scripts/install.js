dee9652f1e4c892352a77fd45ce7ef5e6129194a
Yaw Joseph Etse <yaw.etse@gmail.com>
2014-09-16 20:57:06 -0400

    fixing login bug

--- scripts/install.js
+++ scripts/install.js
@@ -169,11 +169,11 @@ npm.load({
 	}
 	else {
 		npm.commands.install([
-			'periodicjs.ext.admin@1.5.8',
+			'periodicjs.ext.admin@1.5.81',
 			'periodicjs.ext.dbseed@1.5.23',
 			'periodicjs.ext.default_routes@1.5.2',
 			'periodicjs.ext.install@1.5.2',
-			'periodicjs.ext.login@1.5.21',
+			'periodicjs.ext.login@1.5.3',
 			'periodicjs.ext.mailer@1.5.22',
 			'periodicjs.ext.scheduled_content@1.5.2',
 			'periodicjs.ext.user_access_control@1.5.3',
