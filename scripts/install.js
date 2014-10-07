ad21f255a393d839ce332c0c12ae300fa721cf44
Yaw Joseph Etse <yaw.etse@gmail.com>
2014-10-07 01:46:55 -0400

    updated login extension, npm 1.8.81

--- scripts/install.js
+++ scripts/install.js
@@ -185,7 +185,7 @@ npm.load({
 			'periodicjs.ext.dbseed@1.5.3',
 			'periodicjs.ext.default_routes@1.5.3',
 			'periodicjs.ext.install@1.5.3',
-			'periodicjs.ext.login@1.5.3',
+			'periodicjs.ext.login@1.5.4',
 			'periodicjs.ext.mailer@1.5.22',
 			'periodicjs.ext.scheduled_content@1.5.2',
 			'periodicjs.ext.user_access_control@1.5.4',
