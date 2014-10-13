5d9723cdc1c1c41141b5da3232444192ddfdb9f5
Yaw Joseph Etse <yaw.etse@gmail.com>
2014-10-13 17:26:24 -0400

    npm 1.90.11

--- scripts/install.js
+++ scripts/install.js
@@ -201,14 +201,14 @@ npm.load(
 	else {
 		// console.log('npm',npm.config);
 		npm.commands.install([
-			'periodicjs.ext.admin@1.90.20',
+			'periodicjs.ext.admin@1.90.21',
 			'periodicjs.ext.dbseed@1.90.20',
 			'periodicjs.ext.default_routes@1.90.20',
 			'periodicjs.ext.install@1.90.20',
 			'periodicjs.ext.login@1.90.20',
 			'periodicjs.ext.mailer@1.90.20',
 			'periodicjs.ext.scheduled_content@1.90.20',
-			'periodicjs.ext.user_access_control@1.90.20',
+			'periodicjs.ext.user_access_control@1.90.21',
 			],
 		function (err 
 			) {
