f8540bcd7e0661b37fa932f8b15ef6a71ccf2bdc
Ernie Casilla <ecasilla@icloud>
2014-10-20 15:45:47 -0300

    added test dir and some deps

---
+++ test/unit/app/lib/config_spec.js
@@ -0,0 +1,37 @@
+var path = require('path'),
+    config = require(path.resolve(process.cwd(),'app/lib/config.js')),
+    chai = require('chai'),
+    expect = require('chai').expect,
+    configuration = new config();
+    chai.use(require('chai-fs'));
+
+
+describe('A module that loads configurations for express and periodic',function () {
+  describe('The config object',function () {
+    it('should be an object',function (done) {
+      expect(configuration).to.be.an('object');
+      done();
+    });
+    it('should have an intial json config file',function (done) {
+      var defaultPath = configuration.getConfigFilePath('default');
+      expect(defaultPath).to.be.a.file().and.not.empty;
+      done();
+    });
+    it('should be able to read json config files',function (done) {
+       var defaultPath = configuration.getConfigFilePath('default'),
+           envPath = configuration.getConfigFilePath('development');
+           expect(defaultPath).to.be.a.file().with.json;
+           expect(envPath).to.be.a.file().with.json;
+           done();
+    });
+    it('should give you a method to set a config setting',function (done) {
+        configuration.setConfig('hello','world');
+        expect(configuration).to.have.property('hello');
+        done();
+    });
+    it('should get its version from the package json file',function (done) {
+      expect(configuration.version).to.not.eql('1.0.0');
+      done();
+    })
+  });
+});
---
+++ test/unit/app/lib/periodic_spec.js
@@ -0,0 +1,29 @@
+var path = require('path'),
+    periodicjs = require(path.resolve(process.cwd(),'app/lib/periodic.js')),
+    chai = require('chai'),
+    expect = require('chai').expect;
+    chai.use(require('chai-fs'));
+
+'use-strict'
+
+describe('A module that represents a periodic app',function () {
+
+  describe('The periodic object',function () {
+    it('should return an object',function (done) {
+      expect( periodicjs() ).to.be.an('object');
+      done();
+    });
+
+    it('should have the need config properties', function (done) {
+      var periodicConf = periodicjs;
+      expect(periodicConf).to.have.property('name');
+      done();
+    });
+
+    it('uses express methods',function () {
+      //console.log( periodicjs().init.loadConfiguration() )
+      //console.log( periodicjs().app.get('env') )
+      //expect(periodicjs().app.get('env')).to.eql('development');
+    });
+  });
+});
