ae4fdadb91c5e7a4fd4372741b7ba392fa7ccf8c
Yaw Joseph Etse <yaw.etse@gmail.com>
2014-12-30 17:57:11 -0500

    added fix for config settings in theme

--- content/themes/periodicjs.theme.minimal/routes.js
+++ content/themes/periodicjs.theme.minimal/routes.js
@@ -1,14 +1,13 @@
 'use strict';
-// var path = require('path');
 
 module.exports = function (periodic) {
-	// var 
-	// 		itemController = periodic.app.controller.native.item,
 	var themeController = periodic.app.controller.native.theme,
 		themeRouter = periodic.express.Router(),
+		envThemeSettings;
+
+	if (periodic.settings.themeSettings && periodic.settings.themeSettings.settings) {
 		envThemeSettings = periodic.settings.themeSettings.settings[periodic.settings.application.environment];
 
-	if (periodic.settings.themeSettings) {
 		periodic.app.locals.showperiodiccredit = envThemeSettings.showperiodiccredit;
 		periodic.app.locals.showadminlink = envThemeSettings.showadminlink;
 		periodic.app.locals.navlinks = envThemeSettings.navlinks;
@@ -19,12 +18,11 @@ module.exports = function (periodic) {
 		periodic.app.locals.customfooter = envThemeSettings.customfooter;
 	}
 
-	if (envThemeSettings.navlinks === 'categories') {
+	if (envThemeSettings && envThemeSettings.navlinks === 'categories') {
 		themeRouter.get('/browse/:entitytype|/browse/:entitytype/:entityitems|/author/:id|/search|/404|/notfound|/collections|/collection/:id|/collection/search|/items|/item/search|/articles|/item/:id|/', themeController.loadNavCategories, themeController.getNavCategories);
 	}
-	// // create new route to document items to post
+	// create new route to document items to post
 	themeRouter.get('/', themeController.setCacheHeader, themeController.homepagedata, themeController.homepageindex);
-
 
 	periodic.app.use(themeRouter);
 	return periodic;
--- content/themes/periodicjs.theme.reader/routes.js
+++ content/themes/periodicjs.theme.reader/routes.js
@@ -1,28 +1,28 @@
 'use strict';
-// var path = require('path');
 
 module.exports = function (periodic) {
-	var readerthemeController = require('./controller/readertheme')(periodic),
+	var themeController = periodic.app.controller.native.theme,
 		themeRouter = periodic.express.Router(),
+		envThemeSettings;
+
+	if (periodic.settings.themeSettings && periodic.settings.themeSettings.settings) {
 		envThemeSettings = periodic.settings.themeSettings.settings[periodic.settings.application.environment];
 
-	if (periodic.settings.themeSettings) {
 		periodic.app.locals.showperiodiccredit = envThemeSettings.showperiodiccredit;
 		periodic.app.locals.showadminlink = envThemeSettings.showadminlink;
 		periodic.app.locals.navlinks = envThemeSettings.navlinks;
 		periodic.app.locals.autohidetitle = envThemeSettings.autohidetitle;
 		periodic.app.locals.usecustomnav = envThemeSettings.usecustomnav;
 		periodic.app.locals.customnav = envThemeSettings.customnav;
+		periodic.app.locals.usecustomfooter = envThemeSettings.usecustomfooter;
+		periodic.app.locals.customfooter = envThemeSettings.customfooter;
 	}
 
-	if (envThemeSettings.navlinks === 'categories') {
-		themeRouter.get('/browse/:entitytype|/browse/:entitytype/:entityitems|/author/:id|/search|/404|/notfound|/collections|/collection/:id|/collection/search|/items|/item/search|/articles|/item/:id|/', readerthemeController.loadNavCategories, readerthemeController.getNavCategories);
+	if (envThemeSettings && envThemeSettings.navlinks === 'categories') {
+		themeRouter.get('/browse/:entitytype|/browse/:entitytype/:entityitems|/author/:id|/search|/404|/notfound|/collections|/collection/:id|/collection/search|/items|/item/search|/articles|/item/:id|/', themeController.loadNavCategories, themeController.getNavCategories);
 	}
-
-	// require('./scripts/setup')(periodic);
 	// create new route to document items to post
-
-	themeRouter.get('/', readerthemeController.setCacheHeader, readerthemeController.homepagedata, readerthemeController.homepageindex);
+	themeRouter.get('/', themeController.setCacheHeader, themeController.homepagedata, themeController.homepageindex);
 
 	periodic.app.use(themeRouter);
 	return periodic;
