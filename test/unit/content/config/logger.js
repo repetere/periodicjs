6407b3676c78fff38814c5d8dd6b42fcfa4bb94a
Yaw Joseph Etse <yaw.etse@gmail.com>
2016-05-31 10:48:38 -0400

    fixing logger tests

--- test/unit/content/config/logger.js
+++ test/unit/content/config/logger.js
@@ -4,28 +4,28 @@ var path = require('path'),
   Logger = require(path.resolve(process.cwd(), 'content/config/logger.js')),
   fs = require('fs-extra'),
   chai = require('chai'),
-  expect = require('chai').expect,
-  defaultPath,
-  configuration = new Logger({env:'test'});
+  expect = require('chai').expect;
   chai.use(require('@yawetse/chai-fs'));
 
 
 describe('A customizible winston logger', function () {
-  it('should return a winston logger',function(){
+  it('should return a winston logger',function(done){
     let winstonlogger = new Logger('test');
     // console.log('winstonlogger',winstonlogger);
     expect(winstonlogger).to.be.an('object');
     expect(winstonlogger.loggerConfig).to.be.an('object');
+    done();
   });
   it('should ensure that a logging directory exists',function(done){
-    fs.readdir(process.cwd(),function(err,files){
+    fs.readdir(path.join(process.cwd(),'logs'),function(err,files){
       expect(err).to.be.null;
       expect(files).to.be.an('array');
-      done()
-    })
+      done();
+    });
   });
-  it('should be customizible by environment',function(){
+  it('should be customizible by environment',function(done){
     let winstonlogger = new Logger('production');
     expect(winstonlogger.loggerConfig.env).to.equal('production');
+    done();
   });
 });
