bacabb88dec8f6e813992c613a1723732ab5764b
bryan chu <bryan@ip-192-168-1-20.ec2.internal>
2016-06-03 17:52:19 -0400

    updated periodic_spec spacing

--- test/intergration/app/lib/periodic_spec_int.js
+++ test/intergration/app/lib/periodic_spec_int.js
@@ -23,34 +23,34 @@ let periodicjs,
 //   return res.headers['set-cookie'][0].split(';')[0];		 +    this.timeout(10000);
 // }
 
-describe('the default routes when no modules are installed', function () {
+describe('the default routes when no modules are installed', function (){
   this.timeout(10000);
-  before('connect to mongo', function (done) {
-    periodicLib.init({}, function (err, periodicInitialized) {
-      if (err) {
+  before('connect to mongo', function (done){
+    periodicLib.init({}, function (err, periodicInitialized){
+      if(err) {
         done(err);
       } else {
         periodicjs = periodicInitialized;
         request = supertest('http://localhost:' + periodicjs.port),
 
-          periodicjs.mongoose.connection.on('connected', function () {
-            periodicExpressApp = http.createServer(periodicjs.expressapp).listen(periodicjs.port, function () {
+          periodicjs.mongoose.connection.on('connected', function (){
+            periodicExpressApp = http.createServer(periodicjs.expressapp).listen(periodicjs.port, function (){
               number_of_extensions = periodicjs.periodic.settings.extconf.extensions.length;
               for (var x in periodicjs.periodic.settings.extconf.extensions) {
-                if (periodicjs.periodic.settings.extconf.extensions[x].name === 'periodicjs.ext.default_routes' && periodicjs.periodic.settings.extconf.extensions[x].enabled === true) {
+                if(periodicjs.periodic.settings.extconf.extensions[x].name === 'periodicjs.ext.default_routes' && periodicjs.periodic.settings.extconf.extensions[x].enabled === true) {
                   default_routes_extension_enabled = true;
                 }
-                if (periodicjs.periodic.settings.extconf.extensions[x].name === 'periodicjs.ext.asyncadmin' && periodicjs.periodic.settings.extconf.extensions[x].enabled === true) {
+                if(periodicjs.periodic.settings.extconf.extensions[x].name === 'periodicjs.ext.asyncadmin' && periodicjs.periodic.settings.extconf.extensions[x].enabled === true) {
                   admin_extension_enabled = true;
                 }
-                if (periodicjs.periodic.settings.extconf.extensions[x].name === 'periodicjs.ext.install' && periodicjs.periodic.settings.extconf.extensions[x].enabled === true) {
+                if(periodicjs.periodic.settings.extconf.extensions[x].name === 'periodicjs.ext.install' && periodicjs.periodic.settings.extconf.extensions[x].enabled === true) {
                   install_extension_enabled = true;
                 }
-                if (periodicjs.periodic.settings.extconf.extensions[x].enabled === true) {
+                if(periodicjs.periodic.settings.extconf.extensions[x].enabled === true) {
                   number_of_enabled_extensions++;
                 }
               }
-              if (periodicjs.periodic.settings.theme) {
+              if(periodicjs.periodic.settings.theme) {
                 has_custom_theme = true;
               }
               console.log('number_of_extensions', number_of_extensions);
@@ -64,9 +64,9 @@ describe('the default routes when no modules are installed', function () {
       }
     });
   });
-  context('GET /', function () {
-    it('should show the views/home page', function (done) {
-      if (number_of_enabled_extensions === 0 && !has_custom_theme) {
+  context('GET /', function (){
+    it('should show the views/home page', function (done){
+      if(number_of_enabled_extensions === 0 && !has_custom_theme) {
         request
           .get('/')
           .expect('Content-Type', 'text/html; charset=utf-8')
@@ -76,8 +76,8 @@ describe('the default routes when no modules are installed', function () {
         done();
       }
     });
-    it('should respond with 404 it given an unknown route', function (done) {
-      if (install_extension_enabled === false && !has_custom_theme) {
+    it('should respond with 404 it given an unknown route', function (done){
+      if(install_extension_enabled === false && !has_custom_theme) {
         request
           .get('/random-page-not-found-9809j0vnq8hv0h708jv0advnotfound' + new Date())
           .expect('Content-Type', 'text/html; charset=utf-8')
@@ -88,8 +88,8 @@ describe('the default routes when no modules are installed', function () {
         done();
       }
     });
-    it('should respond with json when requesting json', function (done) {
-      if (number_of_enabled_extensions === 0 && !has_custom_theme) {
+    it('should respond with json when requesting json', function (done){
+      if(number_of_enabled_extensions === 0 && !has_custom_theme) {
         request
           .get('/')
           .set('Accept', 'application/json')
@@ -100,10 +100,10 @@ describe('the default routes when no modules are installed', function () {
       }
     });
   });
-  describe('Default Routes are configured correctly', function () {
-    context('Get /articles', function () {
-      it('should have the route article installed', function (done) {
-        if (default_routes_extension_enabled) {
+  describe('Default Routes are configured correctly', function (){
+    context('Get /articles', function (){
+      it('should have the route article installed', function (done){
+        if(default_routes_extension_enabled) {
           request
             .get('/articles')
             .expect('Content-Type', 'text/html; charset=utf-8')
@@ -115,13 +115,13 @@ describe('the default routes when no modules are installed', function () {
     });
   });
 
-  context('Get /p-admin', function () {
-    it('should redirect to login', function (done) {
-      if (admin_extension_enabled) {
+  context('Get /p-admin', function (){
+    it('should redirect to login', function (done){
+      if(admin_extension_enabled) {
         request
           .get('/p-admin')
           .expect('Content-Type', 'text/plain; charset=utf-8')
-          .expect(302, function (err, res) {
+          .expect(302, function (err, res){
             expect(err).to.be.a('null');
             expect(res).to.be.an('object');
             done();
