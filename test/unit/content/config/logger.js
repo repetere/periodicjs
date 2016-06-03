0c8847252aabf38e1385ba8a78084d1aca9d5f18
bryan chu <bryan@ip-192-168-1-20.ec2.internal>
2016-06-03 17:11:03 -0400

    set tab size to 4

--- test/unit/content/config/logger.js
+++ test/unit/content/config/logger.js
@@ -10,23 +10,23 @@ chai.use(require('@yawetse/chai-fs'));
 
 describe('A customizible winston logger', function () {
  it('should return a winston logger', function (done) {
-	let winstonlogger = new Logger('test');
-	expect(winstonlogger).to.be.an('object');
-	// expect(winstonlogger.loggerConfig).to.be.an('object');
-	done();
+  let winstonlogger = new Logger('test');
+  expect(winstonlogger).to.be.an('object');
+  // expect(winstonlogger.loggerConfig).to.be.an('object');
+  done();
  });
  it('should ensure that a logging directory exists', function (done) {
-	fs.readdir(path.join(process.cwd(), 'logs'), function (err, files) {
-	 expect(err).to.be.null;
-	 expect(files).to.be.an('array');
-	 done();
-	});
+  fs.readdir(path.join(process.cwd(), 'logs'), function (err, files) {
+   expect(err).to.be.null;
+   expect(files).to.be.an('array');
+   done();
+  });
  });
  it('should be customizible by environment', function (done) {
-	let winstonlogger = new Logger('production');
-	if (winstonlogger.exceptionHandlers && winstonlogger.exceptionHandlers.file && winstonlogger.exceptionHandlers.file.filename) {
-	 expect(winstonlogger.exceptionHandlers.file.filename.split('-')[0]).to.equal('production');
-	}
-	done();
+  let winstonlogger = new Logger('production');
+  if (winstonlogger.exceptionHandlers && winstonlogger.exceptionHandlers.file && winstonlogger.exceptionHandlers.file.filename) {
+   expect(winstonlogger.exceptionHandlers.file.filename.split('-')[0]).to.equal('production');
+  }
+  done();
  });
 });
