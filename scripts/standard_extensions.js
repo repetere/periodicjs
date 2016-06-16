c46a5aef5720b0ae0598dab0fd42c0a54804449c
Yaw Joseph Etse <yaw.etse@gmail.com>
2016-06-16 14:50:07 -0400

    updating styles

--- scripts/standard_extensions.js
+++ scripts/standard_extensions.js
@@ -14,7 +14,7 @@ let standardExtensions = [
 	'periodicjs.ext.login@7.4.2',
 	'periodicjs.ext.user_access_control@7.0.1',
 	'periodicjs.ext.scheduled_content@6.0.1',
-	'periodicjs.ext.asyncadmin@7.4.0',
+	'periodicjs.ext.asyncadmin@7.4.2',
 	'periodicjs.ext.async_cms@7.1.0',
 	'periodicjs.ext.dbseed@6.0.2',
 ];
