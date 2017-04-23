db447920328c2f9f9620f77a1dc202ad0a25a0ef
Yaw Joseph Etse <yaw.etse@gmail.com>
2017-04-23 17:16:28 -0400

    working on new folder structure

--- test/unit/periodic_spec.js
+++ test/unit/periodic_spec.js
@@ -4,41 +4,41 @@ const path = require('path');
 const events = require('events');
 const chai = require('chai');
 const expect = require('chai').expect;
-let lowkie = require('../../index');
-let lowkieSchema = require('../../lib/schema');
-let lowkieClass = require('../../lib/lowkieClass');
+let periodic = require('../../index');
+// let periodicSchema = require('../../lib/schema');
+let periodicClass = require('../../lib/periodicClass');
 
-describe('Lowkie', function () {
+describe('periodic', function () {
 	describe('Represents a singleton module', function () {
-    it('should always reference the same instance of lowkie when required', function () {
-      let lowkie2 = require('../../index');
-      expect(lowkie)
-        .to.deep.equal(lowkie2)
-        .and.to.be.an.instanceof(lowkieClass);
+    it('should always reference the same instance of periodic when required', function () {
+      let periodic2 = require('../../index');
+      expect(periodic)
+        .to.deep.equal(periodic2)
+        .and.to.be.an.instanceof(periodicClass);
     });
     it('should be implemented with configurable default settings', () => {
-      expect(Object.keys(lowkie.config).length).to.be.greaterThan(0);
+      expect(Object.keys(periodic.config).length).to.be.greaterThan(0);
     });
-    it('should export schema types', () => {
-      expect(lowkie.Schema.Types).to.be.an('object');
-      expect(lowkie.Schema.Types).to.have.property('String');
-      expect(lowkie.Schema.Types.String).to.deep.equal(String);
-      expect(lowkie.Schema.Types).to.have.property('ObjectId');
-    });
-    it('should have connection that emit events', () => {
-      expect(lowkie.connection).to.be.an.instanceof(events.EventEmitter);
-    });
-    it('should expose a method for creating schemas', () => {
-      let testUserSchema = {
-        name: String,
-        email: String,
-        profile: {
-          type: String,
-          default: 'no profile',
-        },
-      };
-      expect(lowkie.Schema).to.be.an.a('function');
-      expect(lowkie.Schema(testUserSchema)).to.be.an.an('object');
-    });
+    // it('should export schema types', () => {
+    //   expect(periodic.Schema.Types).to.be.an('object');
+    //   expect(periodic.Schema.Types).to.have.property('String');
+    //   expect(periodic.Schema.Types.String).to.deep.equal(String);
+    //   expect(periodic.Schema.Types).to.have.property('ObjectId');
+    // });
+    // it('should have connection that emit events', () => {
+    //   expect(periodic.connection).to.be.an.instanceof(events.EventEmitter);
+    // });
+    // it('should expose a method for creating schemas', () => {
+    //   let testUserSchema = {
+    //     name: String,
+    //     email: String,
+    //     profile: {
+    //       type: String,
+    //       default: 'no profile',
+    //     },
+    //   };
+    //   expect(periodic.Schema).to.be.an.a('function');
+    //   expect(periodic.Schema(testUserSchema)).to.be.an.an('object');
+    // });
   });
 });
