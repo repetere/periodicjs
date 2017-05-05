51b5af9972ee03582a3f6f28b6676397e2c77a59
yaw joseph etse <yaw.etse@gmail.com>
2017-05-05 11:47:32 -0400

    working on config loading

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 'use strict';
 require('babel-polyfill');
 require('proxy-polyfill');
+/**
+ * Periodic is an application framework for building enterprise javascript applications.
+ * @exports periodic a reference to the periodic singleton module
+ */
 module.exports = require('./lib/periodic');
