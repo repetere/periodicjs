d97abdb0bbce810bd4cefb33861bc9116bc152cc
Yaw Joseph Etse <yaw.etse@gmail.com>
2017-05-10 16:35:39 -0400

    adding folder structure tests

---
+++ test/unit/init/folder_structure_spec.js
@@ -0,0 +1,98 @@
+'use strict';
+/*jshint expr: true*/
+const path = require('path');
+const events = require('events');
+const chai = require('chai');
+const sinon = require('sinon');
+const fs = require('fs-extra');
+const expect = require('chai').expect;
+const periodic = require('../../../index');
+const periodicClass = require('../../../lib/periodicClass');
+const folderStructure = require('../../../lib/init/folderStructure');
+const testPathDir = path.resolve(__dirname, '../../mock/spec/periodic');
+const initTestPathDir = path.join(testPathDir, 'folderStructureTest');
+const __STRUCTURE_DIR = path.resolve(__dirname, '../../../__STRUCTURE');
+chai.use(require('sinon-chai'));
+chai.use(require('chai-as-promised'));
+
+describe('Periodic Init Folder Structure', function () {
+  this.timeout(10000);
+  before('initialize test periodic dir', (done) => {
+    fs.ensureDir(initTestPathDir)
+      .then(() => {
+        done();
+      }).catch(done);
+  });
+
+  describe('setUpFolderStructure - defines app folders', () => {
+    it('should copy files to app_root', (done) => {
+      const testPeriodic = {
+        config: {
+          app_root: initTestPathDir,
+        },
+      };
+      folderStructure.call(testPeriodic)
+        .then(copiedDir => {
+          return Promise.all([
+            fs.readdir(initTestPathDir),
+            fs.readdir(__STRUCTURE_DIR),
+          ]);
+        })
+        .then(results => {
+          // console.log({ results });
+          expect(results[ 0 ]).to.eql(results[ 1 ]);
+          done();
+        })
+        .catch(done);
+    });
+    it('should not overwrite existing files', (done) => {
+      const testPeriodic = {
+        config: {
+          app_root: initTestPathDir,
+        },
+      };
+      const customConfig = {
+        "configuration": {
+          "type": "db",
+          "db": "lowkie",
+          "options": {
+            "dbpath": "content/config/settings/custom.json"
+          }
+        },
+        "settings": {
+          "name": "My Existing APP"
+        }
+      };
+      const configPath = path.join(initTestPathDir, 'content/config/config.json');
+      fs.outputJson(path.join(initTestPathDir, 'content/config/config.json'), customConfig)
+        .then(() => {
+          return folderStructure.call(testPeriodic)
+        })
+        .then(() => { 
+          return fs.readJson(configPath)
+        })
+        .then(updatedJsonAfterCopy => {
+          expect(updatedJsonAfterCopy).to.eql(customConfig);
+          done();
+        })
+        .catch(done);
+      
+    });
+    it('should return a promise', () => {
+      const testPeriodic = {
+        config: {
+          app_root: initTestPathDir,
+        },
+      };
+      const fsPromise = folderStructure.call(testPeriodic);
+      expect(fsPromise).to.be.a('promise');
+    });
+  });
+  
+  after('remove test periodic dir', (done) => {
+    fs.remove(initTestPathDir)
+      .then(() => {
+        done();
+      }).catch(done);
+  });
+});
