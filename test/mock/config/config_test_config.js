886f3c325ec733d8d07bc4d7d9b565c61d2e6482
Yaw Joseph Etse <yaw.etse@gmail.com>
2017-05-11 13:41:48 -0400

    Adding configuration documentation

--- test/mock/config/config_test_config.js
+++ test/mock/config/config_test_config.js
@@ -4,10 +4,10 @@ module.exports = (options = {}) => {
   return {
     "configuration": {
       "type": "db",
-      "db": "lowkie",
-      "options": {
+      "db": options.db ||"lowkie",
+      "options": Object.assign({
         "dbpath": `${options.dbpathprefix ? options.dbpathprefix+'/' :''}content/config/settings/db.json`,
-      }
+      },options.db_config_options),
     },
     "settings": Object.assign({},
       {
