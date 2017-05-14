0ca0b5348e71f10028341ada8a446738482866b0
yaw joseph etse <yaw.etse@gmail.com>
2017-05-13 20:25:14 -0400

    logger tests

--- test/unit/init/console_spec.js
+++ test/unit/init/console_spec.js
@@ -60,66 +60,6 @@ describe('Periodic Init Console Timer', function() {
       expect(consoleTimer.startTimer()).to.eventually.be.rejected;
       expect(consoleTimer.endTimer()).to.eventually.be.rejected;
     });
-    // it('handles console.timeEnd errors', (done) => {
-    //   try {
-    //     function foo() { throw new Error('Error On console.timeEnd'); }
-    //     var fooSpy = sinon.stub(console, 'timeEnd', foo);
-    //     let newPeriodic = new periodicClass({});
-    //     // process.env.ENV = 'test';
-    //     console.time = sinon.stub(console, 'time');
-    //     console.timeEnd = fooSpy;
-    //     newPeriodic.init({
-    //         debug: false,
-    //         environment: 'DISABLELOG',
-    //         app_root: initTestConsoleTimeEndPathDir,
-    //       }).then((m) => {
-    //         console.time.restore();
-    //         console.timeEnd.restore();
-    //         done(new Error('was not supposed to succeed'));
-    //       })
-    //       .catch((m) => {
-    //         expect(fooSpy.threw()).to.be.ok;
-    //         console.time.restore();
-    //         console.timeEnd.restore();
-    //         done();
-    //       });
-    //   } catch (e) {
-    //     console.time.restore();
-    //     console.timeEnd.restore();
-    //     console.log({ e });
-    //     done();
-    //   }
-    // });
-    // it('handles console.time errors', (done) => {
-    //   try {
-    //     function foo() { throw new Error('Error On console.time'); }
-    //     var fooSpy = sinon.stub(console, 'time', foo);
-    //     let newPeriodic = new periodicClass({});
-    //     // process.env.ENV = 'test';
-    //     console.time = fooSpy;
-    //     console.timeEnd = sinon.stub(console, 'timeEnd');
-    //     newPeriodic.init({
-    //         debug: false,
-    //         environment: 'DISABLELOG',
-    //         app_root: initTestConsoleTimePathDir,
-    //       }).then((m) => {
-    //         console.time.restore();
-    //         console.timeEnd.restore();
-    //         done(new Error('was not supposed to succeed'));
-    //       })
-    //       .catch((m) => {
-    //         expect(fooSpy.threw()).to.be.ok;
-    //         console.timeEnd.restore();
-    //         console.time.restore();
-    //         done();
-    //       });
-    //   } catch (e) {
-    //     console.time.restore();
-    //     console.timeEnd.restore();
-    //     console.log({ e });
-    //     done();
-    //   }
-    // });
   });
   after('remove console test periodic dir', (done) => {
     Promise.all([
