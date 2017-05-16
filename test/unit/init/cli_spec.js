4af2a3f1e810e657011ac8b1f3058b0a9e192d2c
yaw joseph etse <yaw.etse@gmail.com>
2017-05-15 20:15:05 -0400

    adding cli tests

---
+++ test/unit/init/cli_spec.js
@@ -0,0 +1,56 @@
+'use strict';
+/*jshint expr: true*/
+const path = require('path');
+const events = require('events');
+const chai = require('chai');
+const sinon = require('sinon');
+const fs = require('fs-extra');
+const expect = require('chai').expect;
+const periodic = require('../../../index');
+const periodicClass = require('../../../lib/periodicClass');
+const cli = require('../../../lib/init/cli');
+chai.use(require('sinon-chai'));
+chai.use(require('chai-as-promised'));
+
+describe('Periodic Util fetchUtils', function() {
+  this.timeout(10000);
+
+  describe('run', () => {
+    it('should resolve true if not a cli process', () => {
+      const mockThis = {
+        config: {
+          process: {},
+        },
+      };
+      expect(cli.run.call(mockThis)).to.eventually.be.fulfilled;
+    });
+    it('should reject if a cli process', () => {
+      const mockThis = {
+        config: {
+          cli: true,
+          process: {},
+        },
+      };
+      expect(cli.run.call(mockThis)).to.eventually.be.rejected;
+    });
+    it('should notify exit of promise chain', (done) => {
+      const mockThis = {
+        config: {
+          cli: true,
+          process: {},
+        },
+      };
+      cli.run.call(mockThis)
+        .then(result => {
+          done(new Error('it should have rejected cli run'));
+        })
+        .catch(e => {
+          expect(e.message).to.eql('Leave Promise Chain: CLI Process');
+          done();
+        })
+    });
+    it('should handle errors', () => {
+      expect(cli.run()).to.eventually.be.rejected;
+    });
+  });
+});
