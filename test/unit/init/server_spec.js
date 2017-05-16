39dfdaad533a3f38621d7d575f27f6ed3c98921e
yaw joseph etse <yaw.etse@gmail.com>
2017-05-15 21:41:21 -0400

    Adding more server tests

--- test/unit/init/server_spec.js
+++ test/unit/init/server_spec.js
@@ -14,134 +14,6 @@ chai.use(require('chai-as-promised'));
 
 describe('Periodic Init server', function() {
   this.timeout(10000);
-  // describe('onlineEventHandler', () => {
-  //   it('should log process id', () => {
-  //     const infoSpy = sinon.spy();
-  //     const mockThis = {
-  //       logger: {
-  //         info: infoSpy,
-  //       },
-  //     };
-  //     const mockWorker = {
-  //       process: {
-  //         pid: 1,
-  //       },
-  //     };
-  //     cluster.onlineEventHandler.call(mockThis, mockWorker);
-  //     expect(infoSpy.called).to.be.true;
-  //   });
-  // });
-  // describe('exitEventHandler', () => {
-  //   it('should log process id', () => {
-  //     const infoSpy = sinon.spy();
-  //     const forkSpy = sinon.spy();
-  //     const mockThis = {
-  //       logger: {
-  //         info: infoSpy,
-  //       },
-  //     };
-  //     const mockCluster = {
-  //       fork: forkSpy,
-  //     };
-  //     const mockWorker = {
-  //       process: { pid: 1, },
-  //     };
-  //     const mockCode = {};
-  //     const mockSignal = {};
-  //     cluster.exitEventHandler.call(mockThis, mockCluster, mockWorker, mockCluster, mockSignal);
-  //     expect(infoSpy.called).to.be.true;
-  //     expect(forkSpy.called).to.be.true;
-  //   });
-  // });
-  // describe('forkProcess', () => {
-  //   it('should resolve true if not a clustered process', (done) => {
-  //     const mockThis = {
-  //       settings: {
-  //         application: {
-  //           cluster_process: false,
-  //         },
-  //       },
-  //       config: {
-  //         process: {},
-  //       },
-  //     };
-  //     cluster.forkProcess.call(mockThis)
-  //       .then(result => {
-  //         expect(result).to.be.true;
-  //         expect(mockThis.config.process.isClustered).to.be.false;
-  //         done();
-  //       })
-  //       .catch(done);
-  //   });
-  //   it('should cluster master process', (done) => {
-  //     const infoSpy = sinon.spy();
-  //     const eventEmitterSpy = sinon.spy();
-  //     const forkSpy = sinon.spy();
-  //     const clusterObj = {
-  //       isMaster: true,
-  //       on: eventEmitterSpy,
-  //       fork: forkSpy,
-  //     };
-  //     const mockThis = {
-  //       settings: {
-  //         application: {
-  //           cluster_process: true,
-  //         },
-  //       },
-  //       config: {
-  //         process: {},
-  //       },
-  //       logger: {
-  //         info: infoSpy,
-  //       },
-  //       cluster: clusterObj,
-  //     };
-  //     cluster.forkProcess.call(mockThis)
-  //       .then(result => {
-  //         done(new Error('FAILED cluster master test'));
-  //       })
-  //       .catch(e => {
-  //         // expect(mockThis.config.process.isClustered).to.be.true;
-  //         expect(infoSpy.called).to.be.true;
-  //         expect(e.message).to.eql('Leave Promise Chain: Forking Process');
-  //         done();
-  //       });
-  //   });
-  //   it('should cluster fork process', (done) => {
-  //     const infoSpy = sinon.spy();
-  //     const eventEmitterSpy = sinon.spy();
-  //     const forkSpy = sinon.spy();
-  //     const clusterObj = {
-  //       isMaster: false,
-  //       on: eventEmitterSpy,
-  //       fork: forkSpy,
-  //     };
-  //     const mockThis = {
-  //       settings: {
-  //         application: {
-  //           cluster_process: true,
-  //         },
-  //       },
-  //       config: {
-  //         process: {},
-  //       },
-  //       logger: {
-  //         info: infoSpy,
-  //       },
-  //       cluster: clusterObj,
-  //     };
-  //     cluster.forkProcess.call(mockThis)
-  //       .then(result => {
-  //         expect(mockThis.config.process.isClustered).to.be.true;
-  //         expect(result).to.be.true;
-  //         done();
-  //       })
-  //       .catch(done);
-  //   });
-  //   it('should handle errors', () => {
-  //     expect(cluster.forkProcess()).to.eventually.be.rejected;
-  //   });
-  // });
   describe('startSocketIOserver', () => {
     it('should add socketio server', (done) => {
       const infoSpy = sinon.spy();
@@ -185,7 +57,7 @@ describe('Periodic Init server', function() {
           application: {
             server: {
               http: {
-                port: 8780,
+                port: 8773,
               },
             },
           },
@@ -208,11 +80,115 @@ describe('Periodic Init server', function() {
         })
         .catch(done);
     });
+    it('should handle http server creation error', (done) => {
+      const verboseSpy = sinon.spy();
+      const setSpy = sinon.spy();
+      const mockThis = {
+        logger: {
+          verbose: verboseSpy,
+        },
+        settings: {
+          application: {
+            server: {
+              http: {
+                port: 8772,
+              },
+            },
+          },
+        },
+        servers: {
+          set: setSpy,
+        },
+        app: {
+          listen: (port, e) => {
+            e(new Error('test http server error'));
+          },
+        },
+      };
+      server.startHTTPserver.call(mockThis)
+        .then(result => {
+          done(new Error('failed to handle http server errors'));
+        })
+        .catch(e => {
+          expect(e.message).to.eql('test http server error');
+          done();
+        });
+    });
     it('should handle errors', () => {
       expect(server.startHTTPserver()).to.eventually.be.rejected;
     });
   });
   describe('startHTTPSserver', () => {
+    it('should add https server', (done) => {
+      const verboseSpy = sinon.spy();
+      const setSpy = sinon.spy();
+      const mockThis = {
+        logger: {
+          verbose: verboseSpy,
+        },
+        settings: {
+          application: {
+            server: {
+              http: {
+                port: 8770,
+              },
+            },
+          },
+        },
+        servers: {
+          set: setSpy,
+        },
+        app: {
+          listen: (port, e) => {
+            e();
+          },
+        },
+      };
+      server.startHTTPserver.call(mockThis)
+        .then(result => {
+          expect(setSpy.called).to.be.true;
+          expect(verboseSpy.called).to.be.true;
+          expect(result).to.be.true;
+          done();
+        })
+        .catch(done);
+    });
+    // it('should handle https server creation error', (done) => {
+    //   const verboseSpy = sinon.spy();
+    //   const errorSpy = sinon.spy();
+    //   const setSpy = sinon.spy();
+    //   const mockThis = {
+    //     logger: {
+    //       verbose: verboseSpy,
+    //       error: errorSpy,
+    //     },
+    //     settings: {
+    //       application: {
+    //         server: {
+    //           https: {
+    //             port: 8771,
+    //           },
+    //         },
+    //       },
+    //     },
+    //     servers: {
+    //       set: setSpy,
+    //     },
+    //     app: {
+    //       listen: (port, e) => {
+    //         e(new Error('test https server error'));
+    //       },
+    //     },
+    //   };
+    //   server.startHTTPSserver.call(mockThis)
+    //     .then(result => {
+    //       done(new Error('failed to handle http server errors'));
+    //     })
+    //     .catch(e => {
+    //       expect(e.message).to.eql('tests http server error');
+    //       done();
+    //     });
+    // });
     it('should handle errors', () => {
       expect(server.startHTTPSserver()).to.eventually.be.rejected;
     });
