edf2609562c33a8e6be99bdea7f4f82f4b06de7b
Yaw Joseph Etse <yaw.etse@gmail.com>
2017-05-22 17:59:03 -0400

    working on cli tests

--- test/unit/init/cli_spec.js
+++ test/unit/init/cli_spec.js
@@ -14,7 +14,89 @@ chai.use(require('chai-as-promised'));
 
 describe('Periodic Init cli', function() {
   this.timeout(10000);
-
+  describe('processArgv', () => {
+    it('should output status', () => {
+      const infoSpy = sinon.spy();
+      const mockThis = {
+        logger: {
+          info: infoSpy,
+          error:console.error
+        },
+      };
+      const mockOptions = {
+        argv: {
+          status:{},
+        }
+      }
+      cli.processArgv.call(mockThis, mockOptions);
+      expect(infoSpy.called).to.be.true;
+    });
+    it('should handle a --crud command line arguments', () => {
+      const mockThis = new periodicClass();
+      console.log({ mockThis });
+      // const writeSpy = sinon.spy();
+      // const mockThis = {
+      //   logger: {
+      //     // info: infoSpy,
+      //   },
+      // };
+      // const mockOptions = {
+      //   argv: {
+      //     repl: true,
+      //   },
+      //   process: {
+      //     stdout: {
+      //       write: writeSpy,
+      //     },
+      //   },
+      // };
+      // cli.processArgv.call(mockThis, mockOptions);
+      // setTimeout(() => {
+      //   expect(writeSpy.called).to.be.true;
+      //   done();
+      // },1000)
+    });
+    it('should handle a --repl command line argument', (done) => {
+      const writeSpy = sinon.spy();
+      const mockThis = {
+        logger: {
+          // info: infoSpy,
+        },
+      };
+      const mockOptions = {
+        argv: {
+          repl: true,
+        },
+        process: {
+          stdout: {
+            write: writeSpy,
+          },
+        },
+      };
+      cli.processArgv.call(mockThis, mockOptions);
+      setTimeout(() => {
+        expect(writeSpy.called).to.be.true;
+        done();
+      },1000)
+    });
+    it('should handle errors status', () => {
+      const errorSpy = sinon.spy();
+      const exitSpy = sinon.spy();
+      const mockThis = {
+        logger: {
+          error: errorSpy,
+        },
+      };
+      const mockOptions = {
+        process: {
+          exit: exitSpy,
+        },
+      };
+      cli.processArgv.call(mockThis, mockOptions);
+      expect(errorSpy.called).to.be.true;
+      expect(exitSpy.called).to.be.true;
+    });
+  });
   describe('run', () => {
     it('should resolve true if not a cli process', () => {
       const mockThis = {
