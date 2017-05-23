8737b18d33ef9c4941e1520df2bb138ddd3fb1e5
Yaw Joseph Etse <yaw.etse@gmail.com>
2017-05-23 17:55:09 -0400

    extension loading tests

--- test/unit/init/config_databases_spec.js
+++ test/unit/init/config_databases_spec.js
@@ -15,6 +15,7 @@ const initTestPathDir = path.join(testPathDir, 'configTest');
 const CoreData = require('periodicjs.core.data');
 const initTestConfigJsonFile = path.join(initTestPathDir, 'content/config/config.json');
 const configTestConfigJson = require('../../mock/config/config_test_config');
+const extDoc = require('../../mock/config/ext_doc');
 let configPeriodic;
 chai.use(require('sinon-chai'));
 chai.use(require('chai-as-promised'));
@@ -25,10 +26,10 @@ describe('Periodic Init Config', function() {
   before('initialize config test periodic dir', (done) => {
     fs.ensureDir(initTestPathDir)
       .then(() => {
-        return fs.ensureDir(path.join(initTestPathDir, 'content/config'))
+        return fs.ensureDir(path.join(initTestPathDir, 'content/config'));
       })
       .then(() => {
-        return fs.ensureFile(initTestConfigJsonFile)
+        return fs.ensureFile(initTestConfigJsonFile);
       })
       .then(() => {
         return fs.outputJson(initTestConfigJsonFile, configTestConfigJson({
@@ -44,10 +45,10 @@ describe('Periodic Init Config', function() {
         // process.env.ENV = 'test';
         configPeriodic = new periodicClass({});
         configPeriodic.init({
-            app_root: initTestPathDir,
-            cli: true,
-            environment: 'test',
-          })
+          app_root: initTestPathDir,
+          cli: true,
+          environment: 'test',
+        })
           .then(done.bind(done, undefined))
           .catch(done);
       }).catch(done);
@@ -215,7 +216,7 @@ describe('Periodic Init Config', function() {
         periodic_db_name: 'default',
         db_config_type: 'content',
         options: {
-          dbpath: path.join(initTestPathDir,'content/config/settings/config_db.json'),
+          dbpath: path.join(initTestPathDir, 'content/config/settings/config_db.json'),
         },
       };
       const mockThis = {};
@@ -275,26 +276,210 @@ describe('Periodic Init Config', function() {
       expect(config.connectDB()).to.eventually.be.rejected;
     });
     it('should connect to loki', () => {
-      expect(config.connectDB({ db:'lowkie'})).to.eventually.be.rejected;
+      expect(config.connectDB({ db:'lowkie', })).to.eventually.be.rejected;
     });
     it('should connect to mongo', () => {
-      expect(config.connectDB({ db:'mongoose'})).to.eventually.be.rejected;
+      expect(config.connectDB({ db:'mongoose',  })).to.eventually.be.rejected;
     });
     it('should connect to sql', () => {
-      expect(config.connectDB({ db:'sequelize'})).to.eventually.be.rejected;
+      expect(config.connectDB({ db:'sequelize',  })).to.eventually.be.rejected;
     });
-    it('should resolve to true if no db specified', () => {
-      expect(config.connectDB({ db:'other'})).to.eventually.eql(true);
+    it('should resolve to true if no db specified', () => {   
+      expect(config.connectDB({ db:'other',  })).to.eventually.eql(true);
     });
   });
   describe('loadDatabases', () => {
     it('should handle errors', () => {
       expect(config.loadDatabases()).to.eventually.be.rejected;
     });
+    it('should return true if no databases to connect', (done) => {
+      config.loadDatabases.call({
+        settings: {
+          databases: {},
+        },
+      })
+        .then(result => {
+          expect(result).to.be.true;
+          done();
+        })
+        .catch(done);
+    });
+    it('should return connected databases to from settings', (done) => {
+      config.loadDatabases.call({
+        settings: {
+          databases: {
+            test: {
+              db:'other',
+            },
+            test2: {
+              db:'other',
+            },
+          },
+        },
+      })
+        .then(results => {
+          expect(results.length).to.eql(2);
+          done();
+        })
+        .catch(done);
+    });
   });
   describe('loadExtensions', () => {
-    it('should handle errors', () => {
-      expect(config.loadExtensions()).to.eventually.be.rejected;
+    const extNotCompatible = extDoc({ 'periodic_compatibility': '11.0.0', });
+    describe('filterRequiredDependencies', () => {
+      it('should return true if dependency is not optional', () => {
+        expect(config.filterRequiredDependencies({ optional: false, })).to.be.true;
+      });
+      it('should return false if dependency is optional', () => {
+        expect(config.filterRequiredDependencies({ optional: true, })).to.be.false;
+      });
+    });
+    describe('mapForExtensionDependencyName', () => {
+      it('should return extension name', () => {
+        expect(config.mapForExtensionDependencyName({ extname: 'test', })).to.eql('test');
+      });
+    });
+    describe('checkForRequiredExtensions', () => {
+      it('should error if missing required extension', () => { 
+        const errors = [];
+        const ext = {
+          name: 'test extension',
+        };
+        const mockThis = {
+          extensions: new Map(),
+        };
+        const mockOptions = {
+          errors,
+          reqExt: 'requiredExt',
+          ext,
+        };
+        config.checkForRequiredExtensions.call(mockThis, mockOptions);
+        expect(errors).to.have.length.greaterThan(0);
+      });
+      it('should not error if extension map has extension loaded', () => { 
+        const errors = [];
+        const ext = {
+          name: 'requiredExt',
+          version:'1.0.0',
+        };
+        const mockThis = {
+          extensions: new Map(
+            [[ext.name,], ext,]
+          ),
+        };
+        const mockOptions = {
+          errors,
+          reqExt: 'requiredExt',
+          ext,
+        };
+        config.checkForRequiredExtensions.call(mockThis, mockOptions);
+        expect(errors.length).to.eql(0);
+      });
+    });
+    describe('checkExtensionDependencies', () => {
+      it('should error if extension is incompatible with periodic version', () => {
+        const errors = [];
+        const mockThis = {
+          settings: {
+            application: {
+              version:'10.0.0',
+            },
+          },
+        };
+        const mockOptions = {
+          errors, 
+          ext: {
+            periodic_compatibility:'11.0.0',
+          },
+        };
+        config.checkExtensionDependencies.call(mockThis, mockOptions);
+        expect(errors).to.have.length.greaterThan(0);
+      });
+      it('should add valid extension to extensions map', () => {
+        const errors = [];
+        const ext = {
+          name:'testExtension',
+          periodic_compatibility: '10.0.0',
+        };
+        const mockThis = {
+          settings: {
+            application: {
+              version:'10.0.0',
+            },
+          },
+          extensions:new Map(),
+        };
+        const mockOptions = {
+          errors, 
+          ext,
+        };
+        config.checkExtensionDependencies.call(mockThis, mockOptions);
+        expect(mockThis.extensions.has(ext.name)).to.be.true;
+        expect(mockThis.extensions.get(ext.name)).to.eql(ext);
+        expect(errors.length).to.eql(0);
+      });
+    });
+    describe('loadExtensions functionality', () => {
+      it('should handle errors', () => {
+        expect(config.loadExtensions()).to.eventually.be.rejected;
+      });
+      it('should return true if there are no extensions', (done) => {
+        const mockThis = {
+          settings: {
+            application: {
+              exit_on_invalid_extensions:false,
+            },
+          },
+          crud: {
+            ext: {
+              list: () => {
+                return new Promise((resolve, reject) => {
+                  resolve([]);
+                });
+              },
+            },
+          },
+        };
+        config.loadExtensions.call(mockThis)
+          .then(result => {
+            expect(result).to.be.true;
+            done();
+          })
+          .catch(done);
+      });
+      it('should return reject on error if settings is set ', (done) => {
+        const errorSpy = sinon.spy();
+        const mockThis = {
+          settings: {
+            application: {
+              version: '10.0.0',
+              exit_on_invalid_extensions: true,
+            },
+          },
+          extensions: new Set(),
+          crud: {
+            ext: {
+              list: () => {
+                return new Promise((resolve, reject) => {
+                  resolve([extNotCompatible,]);
+                });
+              },
+            },
+          },
+          logger: {
+            error: errorSpy,
+          },
+        };
+        config.loadExtensions.call(mockThis)
+          .then(result => {
+            done(new Error('Should reject if invalid extension config'));
+          })
+          .catch(e => {
+            expect(errorSpy.called).to.be.true;
+            expect(e.message).to.eql('Invalid extension configuration');
+            done();
+          });
+      });
     });
   });
   after('remove config test periodic dir', (done) => {
