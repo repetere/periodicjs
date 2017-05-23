5d10ed476169e669e01f0c85aea6367d27e83e02
yaw joseph etse <yaw.etse@gmail.com>
2017-05-22 22:03:11 -0400

    adding more cli test

--- test/unit/init/cli_spec.js
+++ test/unit/init/cli_spec.js
@@ -15,63 +15,88 @@ chai.use(require('chai-as-promised'));
 describe('Periodic Init cli', function() {
   this.timeout(10000);
   describe('processArgv', () => {
-//     const crud_types = [ 'config', 'ext', 'con' ];
-//     const crud_ops = [ 'create', 'remove', 'update', 'get', 'list' ];
-//     const mockPeriodicThis = new periodicClass();
-//     crud_types.forEach(crud_type => {
-//       crud_ops.forEach(crud_op => {
-//         it(`should handle cli $ node index.js --cli --crud=${crud_type} --crud_op=${crud_op}`, () => {
-//           expect()
-//         });
-//       });
-//     });
-//     /**
-//      * The CRUD CLI has the following options
-// * crud - crud type (entity type: config, ext, con)
-// * crud_op - crud operation (create,remove,update,get,list)
-// * crud_arg - crud argument (argument for crud operation)
-//      */
+    // const crud_types = ['config', 'ext', 'con'];
+    // const crud_ops = ['create', 'remove', 'update', 'get', 'list'];
+    // const mockPeriodicThis = new periodicClass();
+    // mockPeriodicThis.datas.set('configuration', {
+    //   create: Promise.resolve(true),
+    //   delete: Promise.resolve(true),
+    //   update: Promise.resolve(true),
+    //   search: Promise.resolve(true),
+    // });
+    // mockPeriodicThis.datas.set('extension', {
+    //   create: Promise.resolve(true),
+    //   delete: Promise.resolve(true),
+    //   update: Promise.resolve(true),
+    //   search: Promise.resolve(true),
+    // });
+    // crud_types.forEach(crud_type => {
+    //   crud_ops.forEach(crud_op => {
+    //     it(`should handle cli $ node index.js --cli --crud=${crud_type} --crud_op=${crud_op}`, () => {
+    //       expect(cli.processArgv.call(mockPeriodicThis, {
+    //         process: {
+    //           exit: () => {},
+    //         },
+    //         argv: {
+    //           crud: crud_type,
+    //           crud_op,
+    //           crud_arg: {},
+    //         },
+    //       })).to.be.a('promise');
+    //     });
+    //   });
+    // });
     it('should output status', () => {
       const infoSpy = sinon.spy();
       const mockThis = {
         logger: {
           info: infoSpy,
-          error:console.error
+          error: console.error,
         },
       };
       const mockOptions = {
         argv: {
-          status:{},
+          status: {},
         }
       }
       cli.processArgv.call(mockThis, mockOptions);
       expect(infoSpy.called).to.be.true;
     });
-    it('should handle a --crud command line arguments', () => {
-      const mockThis = new periodicClass();
-      // for
-      // console.log({ mockThis });
-      // const writeSpy = sinon.spy();
-      // const mockThis = {
-      //   logger: {
-      //     // info: infoSpy,
-      //   },
-      // };
-      // const mockOptions = {
-      //   argv: {
-      //     repl: true,
-      //   },
-      //   process: {
-      //     stdout: {
-      //       write: writeSpy,
-      //     },
-      //   },
-      // };
-      // cli.processArgv.call(mockThis, mockOptions);
-      // setTimeout(() => {
-      //   expect(writeSpy.called).to.be.true;
-      //   done();
-      // },1000)
+    it('should handle a --crud command line arguments', (done) => {
+      const infoSpy = sinon.spy();
+      const debugSpy = sinon.spy();
+      const mockThis = {
+        crud: {
+          config: {
+            create: () => {
+              // console.log('calling crud', arguments)
+              return new Promise((resolve, reject) => {
+                // console.log('in PROMISE')
+                // expect(infoSpy.called).to.be.true;
+                done();
+                resolve(true);
+              });
+            },
+          },
+        },
+        logger: {
+          info: infoSpy,
+          debug: debugSpy,
+          error: console.error,
+        },
+        config: {},
+      };
+      const mockOptions = {
+        argv: {
+          crud: 'config',
+          crud_op: 'create',
+          crud_arg: {},
+        },
+        process: {
+          exit: () => {},
+        },
+      };
+      cli.processArgv.call(mockThis, mockOptions);
     });
     it('should handle a --repl command line argument', (done) => {
       const writeSpy = sinon.spy();
@@ -94,7 +119,7 @@ describe('Periodic Init cli', function() {
       setTimeout(() => {
         expect(writeSpy.called).to.be.true;
         done();
-      },1000)
+      }, 1000)
     });
     it('should handle errors status', () => {
       const errorSpy = sinon.spy();
