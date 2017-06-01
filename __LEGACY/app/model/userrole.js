b416c7b8a12aecadb37d2e9bb65b4c97f163021c
dany92 <dany@bc.edu>
2017-05-31 20:07:44 -0400

    added mongoose and lowkie models

--- __LEGACY/app/model/userrole.js
+++ __LEGACY/app/model/userrole.js
@@ -8,54 +8,53 @@ const PeriodicSchemaClass = require('./periodic_schema.class.js');
 let schemaMethods = {};
 let schemaStatics = {};
 let schemaModelAttributes = {
-	userroleid: {
-		type: Number,
-		unique: true
-	},
-	title: String,
-	name: {
-		type: String,
-		unique: true
-	},
-	privileges: [{
-		type: ObjectId,
-		ref: 'Userprivilege'
-	}],
-	author: {
-		type: ObjectId,
-		ref: 'User'
-	},
-	description: String,
-	random: Number
+    userroleid: {
+        type: Number,
+        unique: true
+    },
+    title: String,
+    name: {
+        type: String,
+        unique: true
+    },
+    privileges: [{
+        type: ObjectId,
+        ref: 'Userprivilege'
+    }],
+    author: {
+        type: ObjectId,
+        ref: 'User'
+    },
+    description: String,
+    random: Number
 };
 
-let preSaveFunction = function (next, done) {
-	if (this.name !== undefined && this.name.length < 4) {
-		done(new Error('User role title is too short'));
-	}
-	else {
-		next();
-	}
+let preSaveFunction = function(next, done) {
+    if (this.name !== undefined && this.name.length < 4) {
+        done(new Error('User role title is too short'));
+    } else {
+        next();
+    }
 };
 
-class PeriodicSchemaAttributes extends PeriodicSchemaClass.attributes{
-	constructor() {
-		super({
-			entitytype: 'userrole'
-		});
-	}
+class PeriodicSchemaAttributes extends PeriodicSchemaClass.attributes {
+    constructor() {
+        super({
+            entitytype: 'userrole'
+        });
+    }
 }
 
-class userprivilegeModel extends PeriodicSchemaClass.model{
-	constructor(resources) {
-		resources = Object.assign({}, resources);
-		resources.schemaStatics = schemaStatics;
-		resources.schemaMethods = schemaMethods;
-		resources.schemaModelAttributes = schemaModelAttributes;
-		resources.periodicSchemaAttributes = new PeriodicSchemaAttributes();
-		super(resources);
-		this.schema.pre('save', preSaveFunction);
-	}
+class userprivilegeModel extends PeriodicSchemaClass.model {
+    constructor(resources) {
+        resources = Object.assign({}, resources);
+        resources.schemaStatics = schemaStatics;
+        resources.schemaMethods = schemaMethods;
+        resources.schemaModelAttributes = schemaModelAttributes;
+        resources.periodicSchemaAttributes = new PeriodicSchemaAttributes();
+        super(resources);
+        this.schema.pre('save', preSaveFunction);
+    }
 }
 
 module.exports = userprivilegeModel;
